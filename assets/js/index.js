f61adcdd8495b1bce55c12da48195dd6e87f4857
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-18 19:03:54 +0300

    Add Map example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,22 @@
 'use strict';
 
-const sum = (...args) => {
-  return args.reduce((accumulator, currentValue) => {
-    if(!Number.isFinite(currentValue)) {
-      throw new TypeError('Wrong argument type');
-    }
-    return accumulator + currentValue;
-  }, 0);
+const mySymbol = Symbol('test');
+
+const obj = {
+  test: 'test',
+  [mySymbol]: 5
 };
 
-//console.log(sum('text',5,15));
+const obj2 = {
+  test: 'test',
+  [mySymbol]: 5
+};
+
+const myMap = new Map();
+myMap.set(1, {});
+myMap.set('1', true);
+myMap.set(1, 10000);
+myMap.set(obj, 'testValue');
+myMap.set(obj2, 'anotherValue');
+
+console.log(myMap.get(1));
