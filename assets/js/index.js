791e651593ed0bdb5f82356b56f98b9ba4bfaa0a
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-18 19:43:38 +0300

    translate refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,17 +16,14 @@ vocabulary.set('queue', 'очередь');
 const inputStr = 'Cat counT Dog set queue Kernel algOrithm strUcTure of the TABLE';
 
 const translate = (str) => {
-  const lowerString = str.toLowerCase();
-  const splitStrArray = lowerString.split(' ');
-  const translatedStringArray = splitStrArray.map((word)=>{
-    if(vocabulary.has(word)){
-      return vocabulary.get(word);
-    }
-    return word;
-  });
-  const translatedString = translatedStringArray.join(' ');
-
-  return translatedString;
-}
+  return str
+  .trim()
+  .toLowerCase()
+  .split(" ")
+  .map((word) => {
+    return vocabulary.has(word) ? vocabulary.get(word) : word;
+  })
+  .join(" ");
+};
 
 console.log(translate(inputStr));
